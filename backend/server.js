32f1f0f066b4822f64af9d7bbfc171715e2afdc1
areeshasyeda2106 <areeshasyeda2106@gmail.com>
2026-09-01 23:47:44 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
-
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/pages/index.html'));
+});
 // API Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
